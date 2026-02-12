98fee5783059a34d9bf8d18d1612c4d68e777a03
Graham Rounds <gtrounds@gmail.com>
2026-02-11 21:03:35 -0700

    chore: update to v0.1.0

--- katazuke.rb
+++ katazuke.rb
@@ -5,10 +5,10 @@ class Katazuke < Formula
 
   if OS.mac?
     url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.1.0/katazuke-0.1.0-darwin-arm64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 "6ac4ec3a9bb94ba5e4ffd49d47b8d19e51fad2f7cac0ea9175254615c6046680"
   elsif OS.linux?
     url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.1.0/katazuke-0.1.0-linux-amd64.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 "416e6168061f01868a1c73638686104eb7a89db081e501512e718d037929f39e"
   end
 
   def install
