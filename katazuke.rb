d8d410ddce826ee8e3775cfe437cf644851ddf34
Graham Rounds <gtrounds@gmail.com>
2026-02-12 23:52:18 -0700

    chore: update to v0.5.1

--- katazuke.rb
+++ katazuke.rb
@@ -1,14 +1,14 @@
 class Katazuke < Formula
   desc "Developer workspace maintenance tool for tidying up git repositories"
   homepage "https://github.com/agrahamlincoln/katazuke"
-  version "0.5.0"
+  version "0.5.1"
 
   if OS.mac?
-    url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.5.0/katazuke-0.5.0-darwin-arm64.tar.gz"
-    sha256 "42e2d3b60a14061e66200294390b6b740a7985149403019ab97f5d2d694abc10"
+    url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.5.1/katazuke-0.5.1-darwin-arm64.tar.gz"
+    sha256 "9fbe5c45aa05988929ad66d84c2fb8dfaea37fb5779f852e15c34163b0896fe9"
   elsif OS.linux?
-    url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.5.0/katazuke-0.5.0-linux-amd64.tar.gz"
-    sha256 "5db2d6869d274a025b3c0a324148fdf516b4f35f7f67684c64bd4388423844f6"
+    url "https://github.com/agrahamlincoln/katazuke/releases/download/v0.5.1/katazuke-0.5.1-linux-amd64.tar.gz"
+    sha256 "3f77c6b54cc110d56467e4c52d3cad88d50d925c4f02da57962cb748f33f8afa"
   end
 
   def install
